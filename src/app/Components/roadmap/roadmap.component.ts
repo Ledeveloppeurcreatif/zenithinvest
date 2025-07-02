46da6a7b76e9bf2e0f2cd3cc9ca99fa2b0d35811
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-07-02 18:37:58 +0000

    Ajout de la roadmap

--- src/app/Components/roadmap/roadmap.component.ts
+++ src/app/Components/roadmap/roadmap.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-roadmap',
   templateUrl: './roadmap.component.html',
   styleUrl: './roadmap.component.css'
 })
-export class RoadmapComponent {
-
+export class RoadmapComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Initialisation Owl Carousel après le rendu de la vue
+    $(document).ready(() => {
+      $(".roadmap-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText : [
+          '<i class="bi bi-chevron-left"></i>',
+          '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+          0: { items: 1 },
+          576: { items: 2 },
+          768: { items: 3 },
+          992: { items: 4 },
+          1200: { items: 5 }
+        }
+      });
+    });
+  }
 }
--- src/app/Pages/strategies/strategies.component.ts
+++ src/app/Pages/strategies/strategies.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-strategies',
   templateUrl: './strategies.component.html',
   styleUrl: './strategies.component.css'
 })
-export class StrategiesComponent {
-
+export class StrategiesComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Initialisation Owl Carousel après le rendu de la vue
+    $(document).ready(() => {
+      $(".roadmap-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText : [
+          '<i class="bi bi-chevron-left"></i>',
+          '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+          0: { items: 1 },
+          576: { items: 2 },
+          768: { items: 3 },
+          992: { items: 4 },
+          1200: { items: 5 }
+        }
+      });
+    });
+  }
 }
